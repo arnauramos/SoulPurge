a95e2669440b3d981eb6b51a3168fcf1e73f0cc7
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-01-27 13:32:44 +0100

    Player movement, rotation and sprint
    Player movement, rotation and sprint

---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 mousePosition;
+    private Vector2 Movement = new Vector2(0f,0f);
+    private Rigidbody2D Rigidbody;
+    private float delta;
+    public float Speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delta = Time.deltaTime * 1000;
+        PlayerMovement();
+        PlayerAim();
+    }
+
+    void PlayerMovement()
+    {
+        Movement = new Vector2(0f,0f);
+        if (Input.GetKey(KeyCode.W)) Movement.y = Speed * delta;
+        if (Input.GetKey(KeyCode.A)) Movement.x = -Speed * delta;
+        if (Input.GetKey(KeyCode.S)) Movement.y = -Speed * delta;
+        if (Input.GetKey(KeyCode.D)) Movement.x = Speed * delta;
+        if (Input.GetKey(KeyCode.LeftShift)) Movement *= 2f;
+        Rigidbody.velocity = Movement;
+    }
+    void PlayerAim()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.up = mousePosition;
+    }
+}
