dcc7f05ba32e9daf8987b42edd167ef4f0dc04c0
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-01-27 14:19:00 +0100

    Player movement changed and rotation fixed
    Player movement changed and rotation fixed

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,42 +6,42 @@ public class Player : MonoBehaviour
 {
     private Vector2 mousePosition;
     private Vector2 Movement = new Vector2(0f,0f);
-    private Rigidbody2D Rigidbody;
-    private float delta;
+    private Rigidbody2D rb2d;
+    //private float delta;
+    private float fixedDelta;
     public float Speed = 5f;
-    // Start is called before the first frame update
     void Start()
     {
-        Rigidbody = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        delta = Time.deltaTime * 1000;
+        fixedDelta = Time.fixedDeltaTime * 1000;
         PlayerMovement();
         PlayerAim();
     }
 
+    void Update()
+    {
+        //delta = Time.deltaTime * 1000;     
+    }
+
     void PlayerMovement()
     {
-        Movement = new Vector2(0f,0f);
-        if (Input.GetKey(KeyCode.W)) Movement.y = Speed * delta;
-        if (Input.GetKey(KeyCode.A)) Movement.x = -Speed * delta;
-        if (Input.GetKey(KeyCode.S)) Movement.y = -Speed * delta;
-        if (Input.GetKey(KeyCode.D)) Movement.x = Speed * delta;
-        if (Input.GetKey(KeyCode.LeftShift)) Movement *= 2f;
-        Rigidbody.velocity = Movement;
+        Movement.x = Input.GetAxisRaw("Horizontal");
+        Movement.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift)) Speed = 10f;
+        else Speed = 5f; 
+        rb2d.velocity = Movement * Speed * fixedDelta;
     }
     void PlayerAim()
     {
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.up = mousePosition;
+
+        Vector2 lookDirection = mousePosition - rb2d.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb2d.rotation = angle;
     }
 }
