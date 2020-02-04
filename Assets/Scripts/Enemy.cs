5a11507b9beb880bc5f9abf984ea078979887fa1
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-02-04 14:18:45 +0100

    Enemy following basic
    Script of basic following with its speed

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed = 1f;
+    private Vector2 Direction;
+    private Rigidbody2D rb2d;
+    private GameObject Player;
+    float rbx, rby;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Player = GameObject.Find("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        Direction.x = Player.transform.position.x - transform.position.x;
+        Direction.y = Player.transform.position.y - transform.position.y;
+        rb2d.AddForce(Direction.normalized * Speed * Time.fixedDeltaTime,ForceMode2D.Impulse);
+
+        rbx = Mathf.Clamp(rb2d.velocity.x, -Speed, Speed);
+        rby = Mathf.Clamp(rb2d.velocity.y, -Speed, Speed);
+
+        rb2d.velocity = new Vector2(rbx, rby); 
+
+    }
+}
