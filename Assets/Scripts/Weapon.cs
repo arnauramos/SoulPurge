dc39aecb728450714a91df14d60f108231d48955
Alex Barra <48607651+alexbarraboldu@users.noreply.github.com>
2020-02-04 14:31:43 +0100

    ScriptableObjectSetting

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Weapons/WeaponScriptableObject", order = 1)]
+public class Weapon : ScriptableObject
+{
+    public string Name;
+    public Sprite Sprite;
+    public GameObject Bullet;
+    public Vector3 firePoint;
+    public int Ammo;
+    public float Range;
+    public float Damage;
+    public float FireRate;
+}
