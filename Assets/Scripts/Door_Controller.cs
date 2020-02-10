d51f356e8edb2d20e45e0b84cfecaa0f43305f71
Aluminiumgreen <56065068+Aluminiumgreen@users.noreply.github.com>
2020-02-10 14:24:52 +0100

    a

--- Assets/Scripts/Door_Controller.cs
+++ Assets/Scripts/Door_Controller.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Door_Controller : MonoBehaviour
 {
-    Rigidbody2D rb2d;
-    public bool dooropened;
+    public Rigidbody2D rb2d;
+    public GameObject inside;
+    public Sprite S_inside;
+    //public bool dooropened;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        dooropened = false;
+        inside = GameObject.FindGameObjectWithTag("Inside");
+        //dooropened = false;
         
     }
     void Update()
@@ -20,16 +23,17 @@ public class Door_Controller : MonoBehaviour
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        if (collision.gameObject.tag == "Player")
         {
             transform.eulerAngles = Vector3.forward * 90;
-            dooropened = true;
-
+            inside.GetComponent<SpriteRenderer>().sprite = S_inside;
+            //inside.transform.position = new Vector3(inside.transform.position.x, inside.transform.position.y, 0);
+            //dooropened = true;
         }
-        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E) && dooropened == true)
-        {
-            transform.eulerAngles = Vector3.forward * 0;
-            dooropened = false;
-        }
+        //if (collision.gameObject.tag == "Player" && dooropened == true)
+        //{
+        //    transform.eulerAngles = Vector3.forward * 0;
+        //    dooropened = false;
+        //}
     }
 }
