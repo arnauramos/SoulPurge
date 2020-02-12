ccd5244943e6c6a19b682f6d95f528163c75e7b2
Aluminiumgreen <56065068+Aluminiumgreen@users.noreply.github.com>
2020-02-12 08:58:57 +0100

    mapa 0.3.3
    cambio de sprite hecho

--- Assets/Scripts/Door_Controller.cs
+++ Assets/Scripts/Door_Controller.cs
@@ -8,12 +8,12 @@ public class Door_Controller : MonoBehaviour
     public Sprite inside;
     public Sprite outside;
     public GameObject Inside;
+    private Vector3 scaleChange;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-       
-        
+        scaleChange = new Vector3(-0.42f, -0.42f,-1);
     }
     void Update()
     {
@@ -30,6 +30,7 @@ public class Door_Controller : MonoBehaviour
         {
             transform.eulerAngles = Vector3.forward * 90;
             Inside.gameObject.GetComponent<SpriteRenderer>().sprite = inside;
+            Inside.transform.localScale += scaleChange;
 
         }
         if (Input.GetKey(KeyCode.E) && collision.gameObject.tag == "Player")
