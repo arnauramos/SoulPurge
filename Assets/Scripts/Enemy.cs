ae83ed4127460435bacc12054a95615c5aa0c47b
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-02-13 19:46:22 +0100

    AreaOfVision and Dynamic LookRanges

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,17 +6,24 @@ public class Enemy : MonoBehaviour
 {
     public float Speed = 1f;
     public Vector2 Direction;
+    private Vector3 LookingPlayer = new Vector3(120, 120, 1);
+    private Vector3 Idle = new Vector3(75,75,1);
+    private Color blue = new Color(0, 0.5f, 1, 0.2f);
+    private Color red = new Color(1, 0, 0, 0.2f);
     private Rigidbody2D rb2d;
+    private SpriteRenderer AOVsRenderer;
     private GameObject Player;
     private float rbx, rby;
     private float angle;
-    public float LookRange = 30f;
+    public float LookRange = 1.5f;
     public float dmg = 1f;
     public float AttackRate = 10f;
+    public GameObject AreaOfVision;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        AOVsRenderer = AreaOfVision.GetComponent<SpriteRenderer>();
         Player = GameObject.Find("Player");
     }
 
@@ -24,7 +31,19 @@ public class Enemy : MonoBehaviour
     {
         Direction.x = Player.transform.position.x - transform.position.x;
         Direction.y = Player.transform.position.y - transform.position.y;
-        if (Direction.x < LookRange && Direction.x > -LookRange && Direction.y < LookRange && Direction.y > -LookRange) { Movement(); }
+        if (Direction.x < LookRange && Direction.x > -LookRange && Direction.y < LookRange && Direction.y > -LookRange)
+        {
+            LookRange = 2.5f;
+            AreaOfVision.transform.localScale = LookingPlayer;
+            AOVsRenderer.color = red;
+            Movement();
+        }
+        else
+        {
+            LookRange = 1.5f;
+            AreaOfVision.transform.localScale = Idle;
+            AOVsRenderer.color = blue;
+        }
     }
 
     void Movement()
