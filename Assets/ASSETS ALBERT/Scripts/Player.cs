937cb7fbfd1d4f61e59af8a31caf23f57022f699
Aluminiumgreen <56065068+Aluminiumgreen@users.noreply.github.com>
2020-02-14 18:11:30 +0100

    ESTOY TOCANDO SOLO MI BRANCH PORQUE SE ME HA BORRADO EL MAPA, ASI QUE TRBAJARE SOLO MI ESCENA DESDE TU BRANCH HASTA EL MERGE GENERAL DEL DOMINGO

---
+++ Assets/ASSETS ALBERT/Scripts/Player.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private Vector2 mousePosition;
+	private Vector2 Movement;
+	private Rigidbody2D rb2d;
+
+	//private float delta;
+	private float fixedDelta;
+	public float Speed = 0.1f;
+	private float AuxSpeed;
+	public float Sprint = 0.2f;
+
+    public float health = 100f;
+    public float dmgCounter = 100f;
+    private float initialDmgCounter;
+
+	//  VARIABLES FOR GUNS
+	private Rigidbody2D rb2dBullet;
+	private GameObject bulletObject;
+	private float initialBulletTime;
+	private float Counter;
+
+	public int weaponSelected = 0;
+	public Transform firePoint;
+	private static Weapon[] ArrayWeapon;
+	public Weapon weaponUsing;
+
+    public int Rounds;
+    private int AuxRounds;
+    public int Magazines;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		Movement = Vector2.zero;
+		AuxSpeed = Speed;
+
+		//	SET WEAPON STATS TO AUXILIARS
+		ArrayWeapon = WeaponPlaceholder.ArrayWeapon;
+		weaponUsing = ArrayWeapon[weaponSelected];
+        AuxRounds = weaponUsing.Rounds;
+        Rounds = AuxRounds;
+	}
+
+    private void Update()
+    {
+        //	//	SET WEAPON STATS TO AUXILIARS
+        if (health <= 0) Destroy(gameObject);
+        Rounds = weaponUsing.Rounds;
+        Magazines = weaponUsing.Magazines;
+        Reloading();
+    }
+
+	private void FixedUpdate()
+	{
+		fixedDelta = Time.fixedDeltaTime * 1000;
+		Counter = Time.time * fixedDelta;
+		PlayerMovement();
+		PlayerAim();
+		if (Counter >= initialBulletTime && Input.GetMouseButton(0))
+		{
+			Shooting();
+			initialBulletTime = Counter + weaponUsing.FireRate;
+		}
+	}
+
+	void PlayerMovement()
+	{
+		Movement.x = Input.GetAxis("Horizontal");
+		Movement.y = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift) || (Input.GetKey(KeyCode.Keypad0))) Speed = Sprint;
+        else Speed = AuxSpeed;
+
+		if (rb2d.velocity.x > Speed || rb2d.velocity.x < Speed) rb2d.velocity = new Vector2 (0, 0);
+		rb2d.AddForce(Movement * Speed * fixedDelta, ForceMode2D.Impulse);
+	}
+	void PlayerAim()
+	{
+		mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+		Vector2 lookDirection = mousePosition - rb2d.position;
+		float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+		rb2d.rotation = angle;
+	}
+	void Shooting()
+	{
+		bulletObject = Instantiate(weaponUsing.Bullet, firePoint.position, firePoint.rotation);
+		rb2dBullet = bulletObject.GetComponent<Rigidbody2D>();
+		rb2dBullet.AddForce(firePoint.up * weaponUsing.BulletSpeed, ForceMode2D.Impulse);
+		Destroy(bulletObject, weaponUsing.Range);
+        weaponUsing.Rounds--;
+	}
+    void Reloading()
+    {
+        if (Rounds <= 0 && Magazines > 0)
+        {
+            Rounds = AuxRounds;
+            Magazines--;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            dmgCounter = Time.time * fixedDelta;
+            if (dmgCounter >= initialDmgCounter)
+            {
+                health -= collision.gameObject.GetComponent<Enemy>().dmg;
+                initialDmgCounter = dmgCounter + collision.gameObject.GetComponent<Enemy>().AttackRate;
+            }
+        }
+    }
+}
---
+++ Assets/ASSETS ALBERT/Scripts/WeaponPlaceholder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPlaceholder : MonoBehaviour
+{
+	public Weapon[] ArrWeapon;
+
+	public static Weapon[] ArrayWeapon;
+
+	public void Awake()
+	{
+		ArrayWeapon = ArrWeapon;
+	}
+
+	//public WeaponArray_Test[] ArrWeaponTest;
+
+
+	//void Awake()
+	//{
+	//    for (int i = 0; i < ArrWeaponTest.Length; i++)
+	//    {
+	//        ArrWeapon[i].Name = ArrWeaponTest[i].Name;
+	//        ArrWeapon[i].Sprite = ArrWeaponTest[i].Sprite;
+	//        ArrWeapon[i].Bullet = ArrWeaponTest[i].Bullet;
+	//        ArrWeapon[i].firePoint = ArrWeaponTest[i].firePoint;
+	//        ArrWeapon[i].Ammo = ArrWeaponTest[i].Ammo;
+	//        ArrWeapon[i].Range = ArrWeaponTest[i].Range;
+	//        ArrWeapon[i].Damage = ArrWeaponTest[i].Damage;
+	//        ArrWeapon[i].FireRate = ArrWeaponTest[i].FireRate;
+	//    }
+	//}
+}
