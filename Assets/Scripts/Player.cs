ec146b8cb5c92203c1cbfbcb90fc7db85488144e
arnauramos <arnauramoscomas@gmail.com>
2020-02-14 22:06:16 +0100

    Revert "a"
    This reverts commit 88ea6be6d615370d0a9f568bd458cd9de8ded308.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,12 +32,9 @@ public class Player : MonoBehaviour
     public int health = 5;
     public int keys = 0;
 
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> parent of 692eb0d... aasdasd
     void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
@@ -47,7 +44,12 @@ public class Player : MonoBehaviour
 		//	SET WEAPON STATS TO AUXILIARS
 		ArrayWeapon = WeaponPlaceholder.ArrayWeapon;
 		weaponUsing = ArrayWeapon[weaponSelected];
-	}
+
+        //animator = GetComponent<Animator>();
+        //openParamID = Animator.StringToHash("Opening");
+
+        //safedoor.gameObject.GetComponent<Animator>();
+    }
 
 	//private void Update()
 	//{
@@ -101,15 +103,13 @@ public class Player : MonoBehaviour
             Destroy(Object);
         }
 
-        if (collision.gameObject.tag == "Key_Object" && Input.GetKey(KeyCode.E)) 
+        if (collision.gameObject.tag == "Key_Object" && Input.GetKey(KeyCode.E))
         {
             keys++;
             Destroy(Key);
         }
 
-
     }
-<<<<<<< HEAD
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Safe_Door" && keys == 3)
@@ -118,6 +118,4 @@ public class Player : MonoBehaviour
         }
 
     }
-=======
->>>>>>> parent of 692eb0d... aasdasd
 }
