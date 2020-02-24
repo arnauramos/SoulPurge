43416f448fad013b554e68c025eafecddb59f82d
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-02-24 12:29:21 +0100

    Consumables & Upgrades created
    - Created Consumable and Upgrade scriptable object classes
    - Created Ammo, Bandage and Potion as Consumables
    - Created ExtraHealth, ExtraStamina, RegenerationBoost, ResistaceBoost, ShootingBoost and SpeedBoost as Upgrades
    - Inserted placeholder values for Consumables & Upgrades prefabs

---
+++ Assets/Scripts/Consumable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Consumable/ConsumableScriptableObject", order = 1)]
+public class Consumable : ScriptableObject
+{
+    public string Name;
+    public Sprite Sprite;
+    public float Value;
+}
---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Upgrade/UpgradeScriptableObject", order = 1)]
+public class Upgrade : ScriptableObject
+{
+    public string Name;
+    public Sprite Sprite;
+    public float Value;
+    public bool Permanent;
+    public float Duration;
+}
