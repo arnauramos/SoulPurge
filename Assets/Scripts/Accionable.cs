36dce49867e182e751f5a624f09cddd86014b00d
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-02-24 14:26:17 +0100

    Ammo, Bandage and Potion to GO and Accionable
    - Ammo, Bandage and Potion to GameObject
    - Accionable, Gestore and Item 1 as templates (Must delete)

---
+++ Assets/Scripts/Accionable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Accionable : MonoBehaviour
+{
+    public abstract void Usame();
+    public int cantidad;
+}
---
+++ Assets/Scripts/Item1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item1 : Accionable
+{
+    public override void Usame()
+    {
+        Debug.Log("Item010");
+        Debug.Log(this.cantidad);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
