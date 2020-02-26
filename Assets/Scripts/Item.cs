82496edef665dc4943bece81b52d525df08be967
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-02-26 18:51:55 +0100

    Item.cs & Usable.cs created
    Abstract class Item:
    - sprite
    - itemName
    - itemDescription
    - price
    
    Abstract class Usable : Item:
    - Use()
    - ammount
    - doesExpire
    - duration

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+    public Sprite sprite;
+    public string itemName;
+    public string itemDescription;
+    public float price;
+}
---
+++ Assets/Scripts/Usable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Usable : Item
+{
+    public abstract void Use();
+    public int ammount;
+    public bool doesExpire;
+    public float duration;
+}
