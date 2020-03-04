00e7f3bca814402a5f526719f02facc08cd1c17d
AlexBarra <alexbarra00@gmail.com>
2020-03-04 21:03:16 +0100

    New sub Usable class
    -Added a new sub Usable class called None, for putting into the array of objects if there's no item.

---
+++ Assets/Scripts/Usables/None.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class None : Usable
+{
+    //void Start()
+    //{
+    //    //this.sprite;
+    //    //this.itemDescription;
+    //    //this.price;
+    //    //this.ammount;
+    //    //this.doesExpire;
+    //    //this.duration;
+    //}
+    protected override void CheckItem()
+    {
+        if (ammount <= 0) return;
+        ammount--;
+    }
+    public override void Use()
+    {
+        this.itemName = "Potion";
+        this.value = 15;
+
+        CheckItem();
+        PlayerManager.Instance.addHealth(this.value);
+    }
+}
