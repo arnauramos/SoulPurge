87243b52d35abced77554c60006ddfb4919caebf
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-03-09 14:09:16 +0100

    Added HealthBar
    - Added HealthBar to CameraController > MainCamera prefab (temp script)
    - Fixed CameraController minor issues

---
+++ Assets/Scripts/HUD/HealthBarScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarScript : MonoBehaviour
+{
+    Vector2 healthSize = new Vector2(1f, 1f);
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthSize.x > .01f)
+        {
+            healthSize.x -= .01f;
+            transform.localScale = healthSize;
+        }
+    }
+}
