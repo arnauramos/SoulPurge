eca0981b911c5803f398c8650e1c78603d90d78e
arnauramos <50210224+arnauramos@users.noreply.github.com>
2020-03-09 14:27:11 +0100

    HealthBar and StaminaBar implemented

--- Assets/Scripts/HUD/HealthBarScript.cs
+++ Assets/Scripts/HUD/HealthBarScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HealthBarScript : MonoBehaviour
 {
     Vector2 healthSize = new Vector2(1f, 1f);
+    float healthFloat = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,9 @@ public class HealthBarScript : MonoBehaviour
     {
         if (healthSize.x > .01f)
         {
-            healthSize.x -= .01f;
+            healthFloat = PlayerManager.Instance.health / 100;
+            if (healthFloat < 0) healthFloat = 0;
+            healthSize.x = healthFloat;
             transform.localScale = healthSize;
         }
     }
---
+++ Assets/StaminaBarScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaBarScript : MonoBehaviour
+{
+    Vector2 staminaSize = new Vector2(1f, 1f);
+    float staminaFloat = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (staminaSize.x > .01f)
+        {
+            staminaFloat = PlayerManager.Instance.stamina / 200;
+            if (staminaFloat < 0) staminaFloat = 0;
+            staminaSize.x = staminaFloat;
+            transform.localScale = staminaSize;
+        }
+    }
+}
