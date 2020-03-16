69a301a8edcd0164713eed978ca031b4a7ba38c6
arnauramos <arnauramoscomas@gmail.com>
2020-03-16 12:42:02 +0100

    Fixed Health and Stamina bars
    - Changed the way health and stamina bars decrease and increase (Now we modify the fill option from the image component, so the decrease and increase movements are more smooth)

--- Assets/Scripts/HUD/HUDScript.cs
+++ Assets/Scripts/HUD/HUDScript.cs
@@ -8,13 +8,13 @@ public class HUDScript : MonoBehaviour
 {
     // STAMINA
     public GameObject StaminaBar;
-    Vector2 staminaSize = new Vector2(1f, 1f);
-    float staminaFloat = 1f;
+    private Image StaminaImage;
+    private float staminaFloat = 1f;
 
     // HEALTH
     public GameObject HealthBar;
-    Vector2 healthSize = new Vector2(1f, 1f);
-    float healthFloat = 1f;
+    private Image HealthImage;
+    private float healthFloat = 1f;
 
     // SOULS
     public GameObject SoulsCounter;
@@ -37,6 +37,12 @@ public class HUDScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // HEALTH
+        HealthImage = HealthBar.GetComponent<Image>();
+
+        // STAMINA
+        StaminaImage = StaminaBar.GetComponent<Image>();
+
         // SOULS
         SoulsText = SoulsCounter.GetComponent<TextMeshProUGUI>();
 
@@ -62,28 +68,14 @@ public class HUDScript : MonoBehaviour
         Ammo();
     }
     private void Stamina() {
-        if (staminaSize.x > .001f)
-        {
-            staminaFloat = PlayerManager.Instance.stamina / PlayerManager.Instance.maxStamina;
-            if (staminaFloat < 0) staminaFloat = 0;
-            staminaSize.x = staminaFloat;
-            StaminaBar.transform.localScale = staminaSize;
-        }
-        else
-        {
-            staminaFloat = PlayerManager.Instance.stamina / PlayerManager.Instance.maxStamina;
-            staminaSize.x = staminaFloat;
-            StaminaBar.transform.localScale = staminaSize;
-        }
+        staminaFloat = PlayerManager.Instance.stamina / PlayerManager.Instance.maxStamina;
+        if (staminaFloat < 0) staminaFloat = 0;
+        StaminaImage.fillAmount = staminaFloat;
     }
     private void Health() {
-        if (healthSize.x > .001f)
-        {
-            healthFloat = PlayerManager.Instance.health / PlayerManager.Instance.maxHeath;
-            if (healthFloat < 0) healthFloat = 0;
-            healthSize.x = healthFloat;
-            HealthBar.transform.localScale = healthSize;
-        }
+        healthFloat = PlayerManager.Instance.health / PlayerManager.Instance.maxHeath;
+        if (healthFloat < 0) healthFloat = 0;
+        HealthImage.fillAmount = healthFloat;
     }
     private void Souls() {
         SoulsText.text = PlayerManager.Instance.souls.ToString();
