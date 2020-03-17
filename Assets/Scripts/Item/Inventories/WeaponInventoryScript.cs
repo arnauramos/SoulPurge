5fa9df0c37a993d2a16c58ea3cebdde8713bc2c4
arnauramos <arnauramoscomas@gmail.com>
2020-03-17 14:00:12 +0100

    WeaponInventory
    - WeaponInventory in HUD shows Weapons sprites
    - Weapon Slot turns cyan when selected and white when unselected
    - Added the Weapon Slots numbers
    - Created Inventories folder in Scripts and moved WeaponInventoryScript & ItemsInventoryScript there

---
+++ Assets/Scripts/Item/Inventories/WeaponInventoryScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WeaponInventoryScript : MonoBehaviour
+{
+    // SLOTS
+    public List<GameObject> Slots;
+    private Image Border;
+    private SpriteRenderer Spriter;
+    private Sprite auxSprite;
+    private int Selected;
+
+    void Update()
+    {
+        for (int i = 0; i < Slots.Count; i++)
+        {
+            // GETTING COMPONENTS AND DATA
+            auxSprite = PlayerManager.Instance.PlayerGunList[i].sprite;
+            Border = Slots[i].GetComponent<Image>();
+            Spriter = Slots[i].transform.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>();
+
+            // RENDERING INVENTORY SPRITES
+            if (auxSprite != null)
+            {
+                Spriter.sprite = auxSprite;
+            }
+            else
+            {
+                Spriter.sprite = null;
+            }
+
+            // CHANGE BORDER COLOR IF SELECTED
+            Selected = PlayerManager.Instance.weaponSelected;
+            if (i == Selected)
+            {
+                Border.color = Color.cyan;
+            }
+            else
+            {
+                Border.color = Color.white;
+            }
+        }
+
+    }
+}
