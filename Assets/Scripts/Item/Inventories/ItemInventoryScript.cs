14f09251cbf43812ef2e5a8387065122f9b5f08b
arnauramos <arnauramoscomas@gmail.com>
2020-03-18 16:55:40 +0100

    Added BG colors to Inventories
    - Added BG colors to Inventories when selected and unselected
    - Override Main Camera

--- Assets/Scripts/Item/Inventories/ItemInventoryScript.cs
+++ Assets/Scripts/Item/Inventories/ItemInventoryScript.cs
@@ -9,6 +9,7 @@ public class ItemInventoryScript : MonoBehaviour
     // SLOTS
     public List<GameObject> Slots;
     private Image Border;
+    private Image BG;
     private SpriteRenderer Spriter;
     private Sprite auxSprite;
     private int Selected;
@@ -16,6 +17,10 @@ public class ItemInventoryScript : MonoBehaviour
     // SLOTS AMMOUNTS
     public List<TextMeshProUGUI> SlotsAmmounts;
     private int auxAmmount;
+
+    // COLORS
+    private Color32 DarkYellow = new Color32(91, 81, 0, 255);
+    private Color32 LightYellow = new Color32(188, 169, 0, 255);
 
     // Update is called once per frame
     void Update()
@@ -26,6 +31,7 @@ public class ItemInventoryScript : MonoBehaviour
             auxSprite = PlayerManager.Instance.PlayerUsableList[i].sprite;
             auxAmmount = PlayerManager.Instance.PlayerUsableList[i].ammount;
             Border = Slots[i].GetComponent<Image>();
+            BG = Slots[i].transform.GetChild(0).GetComponent<Image>();
             Spriter = Slots[i].transform.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>();
 
             // RENDERING INVENTORY SPRITES
@@ -39,14 +45,17 @@ public class ItemInventoryScript : MonoBehaviour
             }
 
             // CHANGE BORDER COLOR IF SELECTED
+            // CHANGE BG COLOR IF SELECTED
             Selected = PlayerManager.Instance.usableSelected;
             if (i == Selected)
             {
                 Border.color = Color.yellow;
+                BG.color = LightYellow;
             }
             else
             {
                 Border.color = Color.white;
+                BG.color = DarkYellow;
             }
 
             // CHANGE SLOTS AMMOUNTS
--- Assets/Scripts/Item/Inventories/WeaponInventoryScript.cs
+++ Assets/Scripts/Item/Inventories/WeaponInventoryScript.cs
@@ -9,10 +9,14 @@ public class WeaponInventoryScript : MonoBehaviour
     // SLOTS
     public List<GameObject> Slots;
     private Image Border;
+    private Image BG;
     private SpriteRenderer Spriter;
     private Sprite auxSprite;
     private int Selected;
 
+    // COLORS
+    private Color32 DarkBlue = new Color32(0, 24, 91, 255);
+    private Color32 LightBlue = new Color32(0, 50, 200, 255);
     void Update()
     {
         for (int i = 0; i < Slots.Count; i++)
@@ -29,7 +33,7 @@ public class WeaponInventoryScript : MonoBehaviour
             
             Border = Slots[i].GetComponent<Image>();
             Spriter = Slots[i].transform.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>();
-
+            BG = Slots[i].transform.GetChild(0).GetComponent<Image>();
             // RENDERING INVENTORY SPRITES
             if (auxSprite != null)
             {
@@ -41,15 +45,19 @@ public class WeaponInventoryScript : MonoBehaviour
             }
 
             // CHANGE BORDER COLOR IF SELECTED
+            // CHANGE BG COLOR IF SELECTED
             Selected = PlayerManager.Instance.weaponSelected;
             if (i == Selected)
             {
                 Border.color = Color.cyan;
+                BG.color = LightBlue;
             }
             else
             {
                 Border.color = Color.white;
+                BG.color = DarkBlue;
             }
+
         }
 
     }
