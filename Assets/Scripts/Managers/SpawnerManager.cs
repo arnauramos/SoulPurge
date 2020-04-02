ad242d164272529c30a4797a5d2fec06c05ed1ac
AlexBarra <alexbarra00@gmail.com>
2020-04-02 17:53:40 +0200

    SpawnManager 4
    -Finished SpawnerManager.
    -Added the 3 keys on the ZonaHostil scene.
    -Now the player needs the 3 keys and being on the fifth round to be able to go to ZonaSegura.
    -New RegenerationBoost icon.

--- Assets/Scripts/Managers/SpawnerManager.cs
+++ Assets/Scripts/Managers/SpawnerManager.cs
@@ -4,118 +4,163 @@ using UnityEngine;
 
 public class SpawnerManager : MonoBehaviour
 {
-    public static SpawnerManager Instance { get; private set; }
+	public static SpawnerManager Instance { get; private set; }
 
-    public bool STOP;
+	public bool STOP;
 
-    [Header("Map Bounties:")]
-    public Vector2 MapTopLeftLimit;
-    public Vector2 MapBotRightLimit;
+	[Header("Map Bounties:")]
+	public Vector2 MapTopLeftLimit;
+	public Vector2 MapBotRightLimit;
 
-    [Header("Camera Bounties:")]
-    public Vector2 CameraTopLeftLimit;
-    public Vector2 CameraBotRightLimit;
+	[Header("Camera Bounties:")]
+	public Vector2 CameraTopLeftLimit;
+	public Vector2 CameraBotRightLimit;
 
-    [Header("Final Spawn Position:")]
-    public Vector2 NetPosition;
-    private Vector2 RawPosition;
+	[Header("Final Spawn Position:")]
+	public Vector2 NetPosition;
+	private Vector2 RawPosition;
 
-    [Header("Enemies to spawn:")]
-    public Enemy AuxEnemy;
-    public EnemyShooter AuxEnemyShooter;
+	[Header("Enemies to spawn:")]
+	public Enemy AuxEnemy;
+	public EnemyShooter AuxEnemyShooter;
 
-    [Header("Enemy Waves:")]
-    public int ActualRound = 0;
-    bool RoundSpawnComplete;
-    public Vector2Int[] EnemyTypePerRound;
-    public EnemyWaves HostileZoneWaves;
+	[Header("Enemy Waves:")]
+	public int ActualRound = 0;
+	bool RoundSpawnComplete;
+	public EnemyWaves HostileZoneWaves;
 
-    void Start()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(this);
-        }
-        else
-        {
-            Debug.Log("Error: Duplicated " + this + "in the scene");
-        }
-    //EnemyTypePerRound = HostileZoneWaves.EnemyTypePerRound;
-    //AuxActualRound = ActualRound;
+	public float Counter;
+	public float waveCounter;
+	public bool FirstTime;
+
+	void Start()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			Debug.Log("Error: Duplicated " + this + "in the scene");
+		}
+		Counter = 0;
+		FirstTime = true;
 }
 
-    private void Update()
-    {
-        if (!PlayerSceneManager.Instance.ZoneIsHostile)
-        {
-            ActualRound = 0;
-            return;
-        }
-        MapTopLeftLimit = new Vector2(GameObject.Find("West_Wall").transform.position.x, GameObject.Find("North_Wall").transform.position.y);
-        MapBotRightLimit = new Vector2(GameObject.Find("East_Wall").transform.position.x, GameObject.Find("South_Wall").transform.position.y);
+	private void Update()
+	{
+		if (!PlayerSceneManager.Instance.ZoneIsHostile)
+		{
+			ActualRound = 0;
+			FirstTime = true;
+			return;
+		}
+		MapTopLeftLimit = new Vector2(GameObject.Find("West_Wall").transform.position.x, GameObject.Find("North_Wall").transform.position.y);
+		MapBotRightLimit = new Vector2(GameObject.Find("East_Wall").transform.position.x, GameObject.Find("South_Wall").transform.position.y);
 
 
-        CameraTopLeftLimit = new Vector2(Camera.main.transform.position.x - Camera.main.aspect * Camera.main.orthographicSize, Camera.main.transform.position.y + Camera.main.orthographicSize);
-        CameraBotRightLimit = new Vector2(Camera.main.transform.position.x + Camera.main.aspect * Camera.main.orthographicSize, Camera.main.transform.position.y - Camera.main.orthographicSize);
-    }
-    private Vector2 GenerateRawPosition()
-    {
-        return new Vector2(Random.Range(MapTopLeftLimit.x, MapBotRightLimit.x), Random.Range(MapBotRightLimit.y, MapTopLeftLimit.y));
-    }
+		CameraTopLeftLimit = new Vector2(Camera.main.transform.position.x - Camera.main.aspect * Camera.main.orthographicSize, Camera.main.transform.position.y + Camera.main.orthographicSize);
+		CameraBotRightLimit = new Vector2(Camera.main.transform.position.x + Camera.main.aspect * Camera.main.orthographicSize, Camera.main.transform.position.y - Camera.main.orthographicSize);
+	}
+	private Vector2 GenerateRawPosition()
+	{
+		if (PlayerManager.Instance.PlayerPosition.x > 0) //Right
+		{
+			if (PlayerManager.Instance.PlayerPosition.y > 0) //Up
+			{
+				return new Vector2(Random.Range(0, MapBotRightLimit.x), Random.Range(0, MapTopLeftLimit.y));
+			}
+			else //Down
+			{
+				return new Vector2(Random.Range(0, MapBotRightLimit.x), Random.Range(MapBotRightLimit.y, 0));
+			}
+		}
+		else if (PlayerManager.Instance.PlayerPosition.x < 0) //Left
+		{
+			if (PlayerManager.Instance.PlayerPosition.y > 0) //Up
+			{
+				return new Vector2(Random.Range(MapTopLeftLimit.x, 0), Random.Range(0, MapTopLeftLimit.y));
+			}
+			else //Down
+			{
+				return new Vector2(Random.Range(MapTopLeftLimit.x, 0), Random.Range(MapBotRightLimit.y, 0));
+			}
+		}
+		else //All
+		{
+			return new Vector2(Random.Range(MapTopLeftLimit.x, MapBotRightLimit.x), Random.Range(MapBotRightLimit.y, MapTopLeftLimit.y));
+		}
+	}
 
-    public Vector2 CheckRawPosition()
-    {
-        Vector2 RawPosition = GenerateRawPosition();
+	public Vector2 CheckRawPosition()
+	{
+		Vector2 RawPosition = GenerateRawPosition();
 
-        if (STOP) return RawPosition;
+		if (STOP) return RawPosition;
 
-        if ((RawPosition.x >= CameraTopLeftLimit.x && RawPosition.x <= CameraBotRightLimit.x ) && (RawPosition.y >= CameraBotRightLimit.y && RawPosition.y <= CameraTopLeftLimit.y))
-        {
-            RawPosition = CheckRawPosition();
-        }
+		if ((RawPosition.x >= CameraTopLeftLimit.x && RawPosition.x <= CameraBotRightLimit.x) && (RawPosition.y >= CameraBotRightLimit.y && RawPosition.y <= CameraTopLeftLimit.y))
+		{
+			RawPosition = CheckRawPosition();
+		}
 
-        NetPosition = RawPosition;
+		NetPosition = RawPosition;
 
-        //  Enemy Randomize
-        //int EnemyToSpawn = Random.Range(0, 2);
-        //if (EnemyToSpawn == 0) Instantiate(AuxEnemy, NetPosition, Quaternion.identity);
-        //else Instantiate(AuxEnemyShooter, NetPosition, Quaternion.identity);
+		//  Enemy Randomize
+		//int EnemyToSpawn = Random.Range(0, 2);
+		//if (EnemyToSpawn == 0) Instantiate(AuxEnemy, NetPosition, Quaternion.identity);
+		//else Instantiate(AuxEnemyShooter, NetPosition, Quaternion.identity);
 
-        //Debug.Log(NetPosition);
-        return NetPosition;
-    }
+		//Debug.Log(NetPosition);
+		return NetPosition;
+	}
 
-    public void Spawner()
-    {
-        if (RoundSpawnComplete || !PlayerSceneManager.Instance.ZoneIsHostile) return;
+	public void Spawner()
+	{
+		if (RoundSpawnComplete || !PlayerSceneManager.Instance.ZoneIsHostile) return;
 
-        // Enemy Spawn
-        int EnemiesToSpawn = HostileZoneWaves.EnemyTypePerRound[ActualRound].x;
-        for (int m = 0; m < EnemiesToSpawn; m++)
-        {
-            NetPosition = CheckRawPosition();
-            Instantiate(AuxEnemy, NetPosition, Quaternion.identity);
-        }
-        EnemiesToSpawn = HostileZoneWaves.EnemyTypePerRound[ActualRound].y;
-        for (int r = 0; r < EnemiesToSpawn; r++)
-        {
-            NetPosition = CheckRawPosition();
-            Instantiate(AuxEnemyShooter, NetPosition, Quaternion.identity);
-        }
+		if (FirstTime)
+		{
+			if (Counter >= 400f)
+			{
+				FirstTime = false;
+				Counter = 0;
+			}
+			Counter++;
+			return;
+		}
 
-        RoundSpawnComplete = true;
-    }
+		// Enemy Spawn
+		int EnemiesToSpawn = HostileZoneWaves.EnemyTypePerRound[ActualRound].x;
+		for (int m = 0; m < EnemiesToSpawn; m++)
+		{
+			NetPosition = CheckRawPosition();
+			Instantiate(AuxEnemy, NetPosition, Quaternion.identity);
+		}
+		EnemiesToSpawn = HostileZoneWaves.EnemyTypePerRound[ActualRound].y;
+		for (int r = 0; r < EnemiesToSpawn; r++)
+		{
+			NetPosition = CheckRawPosition();
+			Instantiate(AuxEnemyShooter, NetPosition, Quaternion.identity);
+		}
 
-    public void EnemyChecker()
-    {
-        int MeleeEnemies = GameObject.FindObjectsOfType<Enemy>().Length;
-        int RangedEnemies = GameObject.FindObjectsOfType<EnemyShooter>().Length;
+		RoundSpawnComplete = true;
+	}
 
-        if ((MeleeEnemies == 0 && RangedEnemies == 0) && PlayerSceneManager.Instance.ZoneIsHostile)
-        {
-            ActualRound++;
-            RoundSpawnComplete = false;
-        }
-    }
+	public void EnemyChecker()
+	{
+		int MeleeEnemies = GameObject.FindObjectsOfType<Enemy>().Length;
+		int RangedEnemies = GameObject.FindObjectsOfType<EnemyShooter>().Length;
+
+		if ((MeleeEnemies == 0 && RangedEnemies == 0) && PlayerSceneManager.Instance.ZoneIsHostile && !FirstTime)
+		{
+			if (Counter >= 200f)
+			{
+				ActualRound++;
+				RoundSpawnComplete = false;
+				Counter = 0;
+			}
+			Counter +=1f;
+		}
+	}
 }
