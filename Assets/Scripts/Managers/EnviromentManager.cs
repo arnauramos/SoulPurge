8f01927530e73d5c1c15234888a665ff894f9230
alexbarra <alexbarra00@gmail.com>
2020-05-12 11:31:24 +0200

    DoorFixed

--- Assets/Scripts/Managers/EnviromentManager.cs
+++ Assets/Scripts/Managers/EnviromentManager.cs
@@ -39,25 +39,35 @@ public class EnviromentManager : MonoBehaviour
 	{
         if (Input.GetKeyDown(KeyCode.E) || PlayerManager.Instance.speed == PlayerManager.Instance.maxSpeed || PlayerManager.Instance.speed == PlayerManager.Instance.sprint)
         {
-            door.eulerAngles = Vector3.forward * 90;
+			float dX = door.localScale.x * 2.75f;
+			float dY = door.localScale.y * 9f;
+
+			Vector3 aPos = new Vector3(door.position.x - dX, door.position.y + dY, door.position.z);
+			door.eulerAngles = Vector3.forward * 90;
+			door.position = aPos;
 		}
 	}
 	public void closeDoor(Transform door)
 	{
 		if (Input.GetKeyDown(KeyCode.E) || PlayerManager.Instance.speed == PlayerManager.Instance.sprint)
 		{
+			float dX = door.localScale.x * 2.75f;
+			float dY = door.localScale.y * 9f;
+
+			Vector3 aPos = new Vector3(door.position.x + dX, door.position.y - dY, door.position.z);
 			door.eulerAngles = Vector3.forward * 0;
+			door.position = aPos;
 		}
 	}
 
 	public void openRoof()
 	{
 		RoofTilemapTransform = GameObject.Find("Roof-Tilemap").transform;
-		RoofTilemapTransform.position = new Vector3(RoofTilemapTransform.position.x, RoofTilemapTransform.position.y, 1f);
+		RoofTilemapTransform.position = new Vector3(RoofTilemapTransform.position.x, RoofTilemapTransform.position.y, 1.0f);
 	}
 	public void closeRoof()
 	{
 		RoofTilemapTransform = GameObject.Find("Roof-Tilemap").transform;
-		RoofTilemapTransform.position = new Vector3(RoofTilemapTransform.position.x, RoofTilemapTransform.position.y, -3f);
+		RoofTilemapTransform.position = new Vector3(RoofTilemapTransform.position.x, RoofTilemapTransform.position.y, -4.0f);
 	}
 }
